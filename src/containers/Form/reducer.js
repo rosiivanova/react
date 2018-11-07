fcd5581e1cfeab5056821a5006bc92206dbc43e2
Pavel Tsandev <pavel@wmcoding.com>
2018-11-08 00:59:47 +0200

    Remove the default values

--- src/containers/Form/reducer.js
+++ src/containers/Form/reducer.js
@@ -2,10 +2,6 @@ import { FIELD_VALUE_CHANGE, SUBMIT_FORM } from './constants';
 
 const initialState = {
   'results': false,
-  'field_first_name': 'Default value brah',
-  'field_salutation': 1,
-  'field_gift_type[44]': true,
-  'field_gift_type[155]': true,
 };
 
 export default function (state = initialState, action) {
